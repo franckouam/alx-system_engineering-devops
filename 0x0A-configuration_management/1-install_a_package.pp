afe2d2a5a9b5121682fce38b18ac55905f8a03f0
Kouam Franck <kouamfranck96@gmail.com>
2022-09-17 03:34:00 +0100

    0x0A. Configuration management | Execute a command

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # Installs flask
 package { 'flask':
-  ensure   => 'installed',
+  ensure   => '2.1.0',
   provider => 'pip3',
 }
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,5 @@
+# Executes a command
+exec { 'pkill killmenow':
+  path     => '/usr/bin:/usr/sbin:/bin',
+  provider => shell,
+}
