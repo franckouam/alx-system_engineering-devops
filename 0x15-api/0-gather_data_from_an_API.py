1f2ba5104d1577180d4e979bd96ea1a6192fe5f2
Kouam Franck <kouamfranck96@gmail.com>
2022-10-17 23:19:15 +0100

    API Requests

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+script that, using this REST API, for a given employee ID, returns
+information about his/her TODO list progress.
+"""
+
+import requests as re
+import sys
+
+
+def get_todos(user_id):
+    """Get completed TODO of a given user"""
+    todos_info = {}
+    try:
+        res = re.get('https://jsonplaceholder.typicode.com/' +
+                     f'users/{user_id}/todos')
+        todos = res.json()
+        todos_info = {'total': len(todos), 'completed': []}
+        todos_info['name'] = get_username(user_id)
+        for todo in todos:
+            if todo.get('completed'):
+                todos_info['completed'].append(todo.get('title'))
+    except Exception as e:
+        print(e)
+    return todos_info
+
+
+def get_username(user_id):
+    """Get the name of a given user"""
+    name = ""
+    try:
+        res = re.get('https://jsonplaceholder.typicode.com/' +
+                     f'users/{user_id}')
+        name = res.json().get('name')
+    except Exception as e:
+        print(e)
+    return name
+
+
+if __name__ == '__main__':
+    id_ = sys.argv[1]
+    todos = get_todos(id_)
+    completed = todos.get('completed')
+    print('Employee {} is done with tasks({}/{}'.format(
+                    todos.get('name'),
+                    len(completed),
+                    todos.get('total')
+           ))
+    for c in completed:
+        print('\t{}'.format(c))
