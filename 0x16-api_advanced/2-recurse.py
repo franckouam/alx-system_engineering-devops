04f1cf1c21025deb204b1a0a429062ba22e151f5
Kouam Franck <kouamfranck96@gmail.com>
2022-11-02 00:00:26 +0100

    Recursively fetching hot post for a given subreddit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,20 +7,23 @@ URL = 'https://www.reddit.com'
 headers = {"User-Agent": "Custom Agen"}
 
 
-def recursively(uri, after='begining', hot_list=[]):
+def recursively(uri, after=None, hot_list=[]):
     """Recursive function for the job"""
+    parameters = {}
     if after:
-        if after != 'begining':
-            uri = uri + '?after={}'.format(after)
-        res = requests.get(uri,
-                           headers=headers,
-                           allow_redirects=False)
+        parameters['after'] = after
+    res = requests.get(uri,
+                       headers=headers,
+                       allow_redirects=False,
+                       params=parameters)
+    if res.status_code == 200:
         children = res.json().get('data').get('children')
         for child in children:
             hot_list.append(child.get('data').get('title'))
         new_after = res.json().get('data').get('after')
-        return recursively(uri, new_after, hot_list)
-    return hot_list
+        if new_after is not None:
+            recursively(uri, new_after, hot_list)
+        return hot_list
 
 
 def recurse(subreddit, hot_list=[]):
