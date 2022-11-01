9552e1a53cf825d617bfaba2a2185ba4a15f7b5b
Kouam Franck <kouamfranck96@gmail.com>
2022-11-01 16:55:01 +0100

    Fetching top ten hot post for a given subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module for requesting the Reddit API
+"""
+import requests
+URL = 'https://www.reddit.com'
+
+
+def top_ten(subreddit):
+    """Prints the top 10 hot post listed for a given subreddit"""
+    try:
+        endpoint = '/r/{}/hot.json?count=10'.format(subreddit)
+        headers = {"User-Agent": "Custom Agen"}
+        res = requests.get(URL+endpoint,
+                           headers=headers,
+                           allow_redirects=False)
+        children = res.json().get('data').get('children')
+        for i in range(10):
+            print(children[i].get('data').get('title'))
+    except Exception:
+        pass
